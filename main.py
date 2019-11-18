84f8b65f419c0685069660c24bc7866147ad3482
Oleh Datskiv <odatsk@softserveinc.com>
2019-11-18 14:31:54 +0200

    edit scripts to get more than one box

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+import argparse
 import os
 import glob
 import cv2
+import numpy as np
+import sys
 import csv
 import json
 import numpy as np
 from pycocotools import mask
 from skimage import measure
+from pycocotools import coco
 import urllib.request
+
+def bounding_box(points):
+    x_coordinates, y_coordinates = zip(*points)
+    return [int(min(x_coordinates)), int(min(y_coordinates)), int(max(x_coordinates)), int(max(y_coordinates))]
 
 def new_mask_annotation(path_to_mask):
     path_to_masks = glob.glob(os.path.join(path_to_mask, '*.png'))
@@ -21,42 +29,36 @@ def new_mask_annotation(path_to_mask):
         ground_truth_bounding_box = mask.toBbox(encoded_ground_truth)
         contours = measure.find_contours(ground_truth_binary_mask, 0.5)
 
+        bboxes = []
+        for contour in contours:
+            bbox = bounding_box(contour)
+            bboxes.append(bbox)
+
         annotation = {
             "segmentation": [],
             "area": ground_truth_area.tolist(),
             "iscrowd": 0,
             "image_id": image_name,
-            "bbox": ground_truth_bounding_box.tolist(),
+            "bbox": bboxes,
             "category_id": 1,
             "id": index
         }
 
-        if os.path.isfile('Json2/{}.json'.format(image_name)):
-            with open('Json2/{}.json'.format(image_name), 'r+') as my_file:
-                data = json.load(my_file)
-                for contour in contours:
-                    contour = np.flip(contour, axis=1)
-                    segmentation = contour.ravel().tolist()
-                    data['segmentation'].append(segmentation)
-                my_file.seek(0)
-                json.dump(data, my_file)
-                my_file.truncate()
-        else:
-            for contour in contours:
-                contour = np.flip(contour, axis=1)
-                segmentation = contour.ravel().tolist()
-                annotation['segmentation'].append(segmentation)
+        for contour in contours:
+            contour = np.flip(contour, axis=1)
+            segmentation = contour.ravel().tolist()
+            annotation['segmentation'].append(segmentation)
 
-            with open('Json2/{}.json'.format(image_name), 'w') as my_file:
+            with open('RosbagJson/{}.json'.format(image_name), 'w') as my_file:
                 json.dump(annotation, my_file)
 
-if __name__ == "__main__":
-    with open('SoftBall.csv') as csvfile:
+if __name__ == '__main__':
+    with open('rosbag2.csv') as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         index = 0
         for row in readCSV:
             index += 1
-            if index < 0:
+            if index < 1291:
                 continue
             data = row[3]
             if len(data) < 40:
@@ -68,7 +70,8 @@ if __name__ == "__main__":
             opener = urllib.request.build_opener()
             opener.addheaders = [('User-agent', 'Mozilla/5.0')]
             urllib.request.install_opener(opener)
-            urllib.request.urlretrieve(link, "BallReadSoftserveMask/{}.png".format(mask_name))
+            urllib.request.urlretrieve(link, "RosbagMask2/{}.png".format(mask_name))
             print(index)
+            
+    new_mask_annotation("RosbagMask2/")
 
-    new_mask_annotation("BallReadSoftserveMask/")
